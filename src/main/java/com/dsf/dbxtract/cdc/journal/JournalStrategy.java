3e17417f7cc8fe24b934dd4810c596a4227e9218
fsanti68 <fsanti68@hotmail.com>
2016-11-08 19:18:13 -0200

    Added javadocs documentation

--- src/main/java/com/dsf/dbxtract/cdc/journal/JournalStrategy.java
+++ src/main/java/com/dsf/dbxtract/cdc/journal/JournalStrategy.java
@@ -1,7 +1,11 @@
 package com.dsf.dbxtract.cdc.journal;
 
 /**
- * 
+ * There are two ways for control what has been already captured:
+ * <ul>
+ * <li>DELETE = imported data is removed from journal table</li>
+ * <li>WINDOW = DB-Xtract keeps record of last window id, without changing journal data</li>
+ * </ul>
  * @author fabio de santi
  *
  */
